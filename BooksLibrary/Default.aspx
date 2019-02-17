6d5645e7517610ebb2502e249cf8cc43aff5930f
Abhi Sharma <abycoolnz05@gmail.com>
2019-02-17 23:55:21 +1300

    2nd commit

---
+++ BooksLibrary/Default.aspx
@@ -0,0 +1,190 @@
+﻿<%@ Page AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BooksLibrary.Default" Language="C#" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Books</h3> 
+        </div>
+        <div>
+            <asp:GridView ID="gvBooks" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" AutoGenerateColumns="false" ShowFooter="true" DataKeyNames="BooksID" ShowHeaderWhenEmpty="true"
+                
+                OnRowCommand="gvBooks_RowCommand" OnRowEditing="gvBooks_RowEditing" OnRowCancelingEdit="gvBooks_RowCancelingEdit"
+                OnRowUpdating="gvBooks_RowUpdating" OnRowDeleting="gvBooks_RowDeleting">
+
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+
+                <Columns>
+                    <asp:TemplateField HeaderText="Title">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Title") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtTitle" Text='<%# Eval("Title") %>'  runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtTitleFooter"  runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    
+                    <asp:TemplateField HeaderText="Year">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Year") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtYear" Text='<%# Eval("Year") %>'  runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtYearFooter"  runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Author">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Author") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtAuthor" Text='<%# Eval("Author") %>' runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtAuthorFooter" runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Genre">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Genre") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtGenre" Text='<%# Eval("Genre") %>' runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtGenreFooter" runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/images/edit-button-128.png" CommandName="Edit" ToolTip="Edit" Width="20px" Height="20px" runat="server" />
+                            <asp:ImageButton ImageUrl="~/images/deletebtn.png" CommandName="Delete" ToolTip="Delete" Width="20px" Height="20px" runat="server" />
+                        </ItemTemplate>
+                            <EditItemTemplate>
+                                <asp:ImageButton ImageUrl="~/images/save.png" CommandName="Update" ToolTip="Update" Width="20px" Height="20px" runat="server" />
+                                <asp:ImageButton ImageUrl="~/images/cancel.png" CommandName="Cancel" ToolTip="Cancel" Width="20px" Height="20px" runat="server" />
+                            </EditItemTemplate>
+                            <FooterTemplate>
+                                <asp:ImageButton ImageUrl="~/images/add.png" CommandName="AddNew" ToolTip="Add New" Width="20px" Height="20px" runat="server" />
+                            </FooterTemplate>                     
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Label ID="lblSuccessMessage" Text="" runat="server" ForeColor="Green"/>
+             <br />
+            <asp:Label ID="lblErrorMessage" Text="" runat="server" ForeColor="Red"/>
+        </div>
+        <br />
+        <div>
+            <div>
+            <h3>Students</h3> 
+            </div>
+            <asp:GridView ID="gvStudents" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" AutoGenerateColumns="false" ShowFooter="true" DataKeyNames="StudentID" ShowHeaderWhenEmpty="true"
+                
+                OnRowCommand="gvStudents_RowCommand" OnRowEditing="gvStudents_RowEditing" OnRowCancelingEdit="gvStudents_RowCancelingEdit"
+                OnRowUpdating="gvStudents_RowUpdating" OnRowDeleting="gvStudents_RowDeleting">
+
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+
+                <Columns>
+                    <asp:TemplateField HeaderText="First Name">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("FirstName") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtFirstName" Text='<%# Eval("FirstName") %>'  runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtFirstNameFooter"  runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    
+                    <asp:TemplateField HeaderText="Last Name">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("LastName") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtLastName" Text='<%# Eval("LastName") %>'  runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtLastNameFooter"  runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Contact">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Contact") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtContact" Text='<%# Eval("Contact") %>' runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtContactFooter" runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Email">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Email") %>'  runat="server"/>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtEmail" Text='<%# Eval("Email") %>' runat="server"/>
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtEmailFooter" runat="server"/>
+                        </FooterTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/images/edit-button-128.png" CommandName="Edit" ToolTip="Edit" Width="20px" Height="20px" runat="server" />
+                            <asp:ImageButton ImageUrl="~/images/deletebtn.png" CommandName="Delete" ToolTip="Delete" Width="20px" Height="20px" runat="server" />
+                        </ItemTemplate>
+                            <EditItemTemplate>
+                                <asp:ImageButton ImageUrl="~/images/save.png" CommandName="Update" ToolTip="Update" Width="20px" Height="20px" runat="server" />
+                                <asp:ImageButton ImageUrl="~/images/cancel.png" CommandName="Cancel" ToolTip="Cancel" Width="20px" Height="20px" runat="server" />
+                            </EditItemTemplate>
+                            <FooterTemplate>
+                                <asp:ImageButton ImageUrl="~/images/add.png" CommandName="AddNew" ToolTip="Add New" Width="20px" Height="20px" runat="server" />
+                            </FooterTemplate>                     
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Label ID="lblSuccess" Text="" runat="server" ForeColor="Green"/>
+             <br />
+            <asp:Label ID="lblError" Text="" runat="server" ForeColor="Red"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ BooksLibrary/Default.aspx.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace BooksLibrary
+{
+    public partial class Default : System.Web.UI.Page          //Connection String to connect to the db
+    {
+        string connectionString = @"Data Source=ABHIMONIKA\SQLEXPRESS;Initial Catalog=BooksLibrary;Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateGridviewBooks();                            //Functions
+                PopulateGridviewStudents();
+            }
+        }
+
+             void PopulateGridviewBooks()
+
+        {
+            DataTable dtbl = new DataTable();                                               //Datatable
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Books",sqlCon);    //Open Sql Connection
+                sqlDa.Fill(dtbl);
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvBooks.DataSource = dtbl;
+                gvBooks.DataBind();
+            }
+            else
+            {
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvBooks.DataSource = dtbl;                                                  //Add rows to datatable
+                gvBooks.DataBind();
+                gvBooks.Rows[0].Cells.Clear();
+                gvBooks.Rows[0].Cells.Add(new TableCell());
+                gvBooks.Rows[0].Cells[0].Text = " No Data Found..!";
+                gvBooks.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+
+            }
+            
+        }
+
+        protected void gvBooks_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {                                                                             //Add new Rows to Books Db
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        string query = "INSERT INTO Books(Title,Year,Author,Genre) VALUES (@Title,@Year,@Author,@Genre)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@Title", (gvBooks.FooterRow.FindControl("txtTitleFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Year", (gvBooks.FooterRow.FindControl("txtYearFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Author", (gvBooks.FooterRow.FindControl("txtAuthorFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Genre", (gvBooks.FooterRow.FindControl("txtGenreFooter") as TextBox).Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        PopulateGridviewBooks();
+                        lblSuccessMessage.Text = " New Record Added";
+                        lblErrorMessage.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        protected void gvBooks_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvBooks.EditIndex = e.NewEditIndex;                                                     //Edit Rows  to Books Db
+            PopulateGridviewBooks();
+        }
+
+        protected void gvBooks_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvBooks.EditIndex = -1;                                                                 //Cancel Edit 
+            PopulateGridviewBooks();
+        }
+
+        protected void gvBooks_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {              
+                   using (SqlConnection sqlCon = new SqlConnection(connectionString))                //Update Rows in Books Db
+                {
+                        sqlCon.Open();
+                        string query = "UPDATE Books SET Title=@Title, Year=@Year, Author=@Author, Genre=@Genre WHERE BooksID = @id";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@Title", (gvBooks.Rows[e.RowIndex].FindControl("txtTitle") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Year", (gvBooks.Rows[e.RowIndex].FindControl("txtYear") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Author", (gvBooks.Rows[e.RowIndex].FindControl("txtAuthor") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Genre", (gvBooks.Rows[e.RowIndex].FindControl("txtGenre") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvBooks.DataKeys[e.RowIndex].Value.ToString()));
+                        sqlCmd.ExecuteNonQuery();
+                        gvBooks.EditIndex = -1;
+                        PopulateGridviewBooks();
+                        lblSuccessMessage.Text = " Selected Rows Updated";
+                        lblErrorMessage.Text = "";
+                    }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        protected void gvBooks_RowDeleting(object sender, GridViewDeleteEventArgs e)        //Delete Rows In Books Db
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE From Books  WHERE BooksID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+  
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvBooks.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridviewBooks();
+                    lblSuccessMessage.Text = " Selected Rows Deleted!";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        void PopulateGridviewStudents()
+
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlData = new SqlDataAdapter("SELECT * FROM Students", sqlCon);    //Students Db
+                sqlData.Fill(dtbl);
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvStudents.DataSource = dtbl;
+                gvStudents.DataBind();
+            }
+            else
+            {
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvStudents.DataSource = dtbl;
+                gvStudents.DataBind();
+                gvStudents.Rows[0].Cells.Clear();
+                gvStudents.Rows[0].Cells.Add(new TableCell());
+                gvStudents.Rows[0].Cells[0].Text = " No Data Found..!";
+                gvStudents.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+
+            }
+        }
+
+        protected void gvStudents_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {           
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))              //Add rows in Students Db
+                    {
+                        sqlCon.Open();
+                        string query = "INSERT INTO Students(FirstName,LastName,Contact,Email) VALUES (@FirstName,@LastName,@Contact,@Email)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@FirstName", (gvStudents.FooterRow.FindControl("txtFirstNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@LastName", (gvStudents.FooterRow.FindControl("txtLastNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Contact", (gvStudents.FooterRow.FindControl("txtContactFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Email", (gvStudents.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        PopulateGridviewBooks();
+                        lblSuccess.Text = " New Record Added";
+                        lblError.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccess.Text = " New Row Inserted";
+                lblError.Text = ex.Message;
+            }
+        }
+
+        protected void gvStudents_RowEditing(object sender, GridViewEditEventArgs e)        //Select Rows to Edit
+        {
+            gvStudents.EditIndex = e.NewEditIndex;
+            PopulateGridviewStudents();
+        }
+
+        protected void gvStudents_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)  //Cancel Row Editing
+        {
+            gvStudents.EditIndex = -1;
+            PopulateGridviewStudents();
+        }
+
+        protected void gvStudents_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))          //Update Selected Rows in Students db
+                {
+                    sqlCon.Open();
+                    string query = "UPDATE Students SET FirstName=@FirstName, LastName=@LastName, Contact=@Contact, Email=@Email WHERE StudentID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@FirstName", (gvStudents.Rows[e.RowIndex].FindControl("txtFirstName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastName", (gvStudents.Rows[e.RowIndex].FindControl("txtLastName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Contact", (gvStudents.Rows[e.RowIndex].FindControl("txtContact") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Email", (gvStudents.Rows[e.RowIndex].FindControl("txtEmail") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvStudents.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    gvStudents.EditIndex = -1;
+                    PopulateGridviewBooks();
+                    lblSuccessMessage.Text = " Selected Rows Updated";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccess.Text = "";
+                lblError.Text = ex.Message;
+            }
+        }
+
+        protected void gvStudents_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();                                                              //Delete From Students table in db
+                    string query = "DELETE From Students  WHERE StudentID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvStudents.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridviewStudents();
+                    lblSuccess.Text = " Selected Rows Deleted!";
+                    lblError.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccess.Text = "";
+                lblError.Text = ex.Message;
+            }
+        }
+    }
+
+
+
+
+    
+
+        
+ 
+}
